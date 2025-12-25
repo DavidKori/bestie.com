b439bac677efa49c6cdbf295a75e1da3c74daec5
KoriDavid <mutugidavid37@gmail.com>
2025-12-26 02:17:31 +1100

    solve server not working 1

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL ='http://localhost:5000/api';
-const API_BASE_URL ='https://create-bestie-backend.onrender.com';
+const API_BASE_URL ='https://create-bestie-backend.onrender.com/api';
 
 // Create axios instance with base config
 const api = axios.create({
